302a93987cf2c392904f1dd1fd69b933d254757c
Konrad Lindenbach <konrad.lindenbach@gmail.com>
2013-08-04 22:35:31 -0400

    added context listener

---
+++ src/chatServer/ChatListener.java
@@ -0,0 +1,29 @@
+package chatServer;
+
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class ChatListener implements ServletContextListener {
+
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+        // This manually deregisters JDBC driver, which prevents Tomcat 7 from complaining about memory leaks wrto this class
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()) {
+            Driver driver = drivers.nextElement();
+            try {
+                DriverManager.deregisterDriver(driver);
+            } catch (SQLException e) {
+                System.out.println("Error deregistering driver " + driver);
+            }
+        }
+	}
+
+	@Override
+	public void contextInitialized(ServletContextEvent arg0) {
+	}
+}
